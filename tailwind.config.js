1adafe5bda033abf000c9c4883caa8d79d2cc340
Chris Kottom <chris@chriskottom.com>
2022-02-24 12:40:10 +0100

    Get Webpack working as an external pipeline 🎉

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  darkMode: "class",
+  content: ["./source/**/*.{erb,html,js}"],
+  theme: {
+    fontFamily: {
+      'sans': ['Lato', '"Helvetica Neue"', '"Noto Sans"', 'Arial', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
