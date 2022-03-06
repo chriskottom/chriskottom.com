cba22bbc7a64dc477518f054622db1250ce98054
Chris Kottom <chris@chriskottom.com>
2022-03-06 19:11:15 +0100

    Null check on dark mode button

--- source/javascripts/darkmode.js
+++ source/javascripts/darkmode.js
@@ -1,11 +1,15 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    document.getElementById('dark-mode-button').addEventListener('click', () => {
-        if (localStorage.getItem('darkMode') === 'true') {
-            localStorage.setItem('darkMode', false)
-            document.documentElement.classList.remove('dark')
-        } else {
-            localStorage.setItem('darkMode', true)
-            document.documentElement.classList.add('dark')
-        }
-    })
+    const button = document.getElementById('dark-mode-button')
+
+    if (button) {
+        button.addEventListener('click', () => {
+            if (localStorage.getItem('darkMode') === 'true') {
+                localStorage.setItem('darkMode', false)
+                document.documentElement.classList.remove('dark')
+            } else {
+                localStorage.setItem('darkMode', true)
+                document.documentElement.classList.add('dark')
+            }
+        })
+    }
 })
